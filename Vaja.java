70adfbedd612d2615f90f17fa56a3f78fcd4d6b5
Tim Kobal <tim.kobal@scng.si>
2026-09-09 09:19:47 +0200

    vaje rekurzija

--- Vaja.java
+++ Vaja.java
@@ -3,13 +3,32 @@ public class Vaja{
 
 
 
-        System.out.println("Rad imam Javo");
-        
+        System.out.println("2 na 10 = "+potenca(10));
+        System.out.println("7 fib število =" +  fib(7));
 
 
 
 
 
+
+    }
+    public static int potenca(int n){
+        //izracuna 2 na na rekurzivno
+
+        if(n==0){
+            return 1;
+        }
+
+        return 2*potenca(n-1);
+    }
+    public static int fib(int n){
+        // izrcuna n-tu fibinacijevo število
+
+        if(n==1||n==2){
+            return 1;
+        }
+
+        return fib(n-1)+ fib(n-2);
 
     }
 }
